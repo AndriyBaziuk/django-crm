08a35874980aa2b00c824af81f3e25bef344de00
Andrii Baziuk <baziukar@gmail.com>
2023-07-02 02:01:10 +0300

    Lead detail view

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import LeadListView
+from .views import LeadDetailView, LeadListView
 
 app_name = 'leads'
 
 urlpatterns = [
     path('', LeadListView.as_view(), name='lead-list'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -7,3 +7,9 @@ class LeadListView(generic.ListView):
     template_name = 'leads/lead_list.html'
     queryset = Lead.objects.all()
     context_object_name = 'leads'
+
+
+class LeadDetailView(generic.DetailView):
+    template_name = 'leads/lead_detail.html'
+    queryset = Lead.objects.all()
+    context_object_name = 'lead'
