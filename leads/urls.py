f1d7c98d0e3951bcd27c9b5492897ea81b1dff7e
Andrii Baziuk <baziukar@gmail.com>
2023-07-05 02:05:27 +0300

    Lead create view

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import LeadDetailView, LeadListView
+from .views import LeadCreateView, LeadDetailView, LeadListView
 
 app_name = 'leads'
 
 urlpatterns = [
     path('', LeadListView.as_view(), name='lead-list'),
     path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
+    path('create/', LeadCreateView.as_view(), name='lead-create'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import reverse
 from django.views import generic
 
+from .forms import LeadModelForm
 from .models import Lead
 
 
@@ -13,3 +15,11 @@ class LeadDetailView(generic.DetailView):
     template_name = 'leads/lead_detail.html'
     queryset = Lead.objects.all()
     context_object_name = 'lead'
+
+
+class LeadCreateView(generic.CreateView):
+    template_name = 'leads/lead_create.html'
+    form_class = LeadModelForm
+
+    def get_success_url(self):
+        return reverse('leads:lead-list')
